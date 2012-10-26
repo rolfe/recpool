f20fa2876c158e261347e85020c8351acac6d231
Jason Rolfe <rolfe@cs.nyu.edu>
2012-10-26 18:31:10 -0400

    Push a test with classification loss scaling = 0.2, and learning rate decay reset after the unsupervised pretraining ista sparsity = 0 group sparsity = 3 encoding_feature_extraction_dictionary unnormalized squared weight matrix on encoding_pooling_dictionary learning rate 2e-3 FixedShrink in the encoder stack

--- run_recpool_net.lua
+++ run_recpool_net.lua
@@ -15,9 +15,9 @@ cmd:option('-num_layers','1', 'number of reconstruction pooling layers in the ne
 cmd:option('-full_test','quick_train', 'train slowly over the entire training set (except for the held-out validation elements)')
 cmd:option('-data_set','train', 'data set on which to perform experiment experiments')
 
-local quick_train_learning_rate = 2e-3 --(1/6)*2e-3 --2e-3 --5e-3
+local quick_train_learning_rate = 5e-3 --(1/6)*2e-3 --2e-3 --5e-3
 local full_train_learning_rate = 2e-3
-local quick_train_epoch_size = 5000
+local quick_train_epoch_size = 500
 
 local fe_layer_size = 200 --400 --200
 local p_layer_size = 50 --200 --50
@@ -53,8 +53,8 @@ mask_mag = 0.3e-2 --0.2e-2 --0.3e-2 --0.4e-2 --0.5e-2 --0 --0.75e-2 --0.5e-2 --0
 --mask_mag = 0
 
 if not(disable_pooling) and not(disable_pooling_losses) then
-   --sl_mag = 0
-   sl_mag = 1e-2 -- attempt to duplicate good run on 10/11
+   sl_mag = 0 -- this *SHOULD* be scaled by L1_scaling
+   --sl_mag = 1e-2 -- attempt to duplicate good run on 10/11
    --sl_mag = 0.025e-2 -- used in addition to group sparsity
 else
    sl_mag = 3e-2
@@ -133,7 +133,7 @@ local lambdas = {ista_L2_reconstruction_lambda = rec_mag, ista_L1_lambda = sl_ma
 
 -- reduce lambda scaling to 0.15; still too sparse
 -- FOR THE LOVE OF GOD!!!  DEBUG ONLY!!!  L1_scaling should also scale sl_mag, but this has been removed to reconstruct the good run on 10/11
-local lambdas_1 = {ista_L2_reconstruction_lambda = rec_mag, ista_L1_lambda = sl_mag, pooling_L2_shrink_reconstruction_lambda = pooling_rec_mag, pooling_L2_orig_reconstruction_lambda = pooling_orig_rec_mag, pooling_L2_shrink_position_unit_lambda = pooling_shrink_position_L2_mag, pooling_L2_orig_position_unit_lambda = pooling_orig_position_L2_mag, pooling_output_cauchy_lambda = L1_scaling * pooling_sl_mag, pooling_mask_cauchy_lambda = L1_scaling * mask_mag} -- classification implicitly has a scaling constant of 1
+local lambdas_1 = {ista_L2_reconstruction_lambda = rec_mag, ista_L1_lambda = L1_scaling * sl_mag, pooling_L2_shrink_reconstruction_lambda = pooling_rec_mag, pooling_L2_orig_reconstruction_lambda = pooling_orig_rec_mag, pooling_L2_shrink_position_unit_lambda = pooling_shrink_position_L2_mag, pooling_L2_orig_position_unit_lambda = pooling_orig_position_L2_mag, pooling_output_cauchy_lambda = L1_scaling * pooling_sl_mag, pooling_mask_cauchy_lambda = L1_scaling * mask_mag} -- classification implicitly has a scaling constant of 1
 
 
 -- NOTE THAT POOLING_MASK_CAUCHY_LAMBDA IS MUCH LARGER
@@ -286,11 +286,13 @@ for i = 1,num_epochs_no_classification do
    end
 
    trainer:train(data)
+   --print('iterations so far: ' .. trainer.config.evalCounter)
    plot_filters(opt, i, model.filter_list, model.filter_enc_dec_list, model.filter_name_list)
 end
 
 -- reset lambdas to be closer to pure top-down fine-tuning and continue training
-model:reset_classification_lambda(1) -- 0.2 seems to strike an even balance between reconstruction and classification
+model:reset_classification_lambda(0.2) -- 0.2 seems to strike an even balance between reconstruction and classification
+trainer.config.evalCounter = 0 -- reset counter for learning rate decay
 num_epochs = 500
 for i = 1+num_epochs_no_classification,num_epochs+num_epochs_no_classification do
    if (i % 20 == 1) and (i > 1) then
