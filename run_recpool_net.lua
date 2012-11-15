5411c0ecd9e4d4c5b146378322e30b713305c071
Jason Rolfe <rolfe@cs.nyu.edu>
2012-11-15 11:25:12 -0500

    Last push didn't set the number of pretraining epochs to 0; this one corrects that.

--- run_recpool_net.lua
+++ run_recpool_net.lua
@@ -16,11 +16,11 @@ cmd:option('-full_test','quick_train', 'train slowly over the entire training se
 cmd:option('-data_set','train', 'data set on which to perform experiment experiments')
 
 local desired_minibatch_size = 1 -- 0 does pure matrix-vector SGD, >=1 does matrix-matrix minibatch SGD
-local quick_train_learning_rate = math.max(1, desired_minibatch_size) * 5e-3 --25e-3 --(1/6)*2e-3 --2e-3 --5e-3
+local quick_train_learning_rate = math.max(1, desired_minibatch_size) * 2e-3 --25e-3 --(1/6)*2e-3 --2e-3 --5e-3
 local full_train_learning_rate = math.max(1, desired_minibatch_size) * 2e-3 --10e-3
 local quick_train_epoch_size = 5000
 
-local num_epochs_no_classification = 200 --200 --501 --201
+local num_epochs_no_classification = 0 --200 --501 --201
 local num_epochs = 1000
 
 local fe_layer_size = 200 --400 --200
